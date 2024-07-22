455aa9254ac0790dc9408ce371ffcba04d54ff1f
trandaison <sontd.it@gmail.com>
2024-07-22 19:38:47 +0700

    feat: Add version command

--- src/bin/changeloger.ts
+++ src/bin/changeloger.ts
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 import mri from 'mri';
 
+const commands = {
+  _default: () => import('../main'),
+  version: () => import('../version'),
+};
+
 async function start() {
   let argv = process.argv.slice(2);
-  argv = mri(argv);
-  import('../main').then((module) => {
-    module.default(argv as any);
+  let command: keyof typeof commands = argv[0] as any;
+  if (!command) {
+    command = '_default';
+  }
+  const args = mri(argv, {
+    alias: {
+      v: ['version'],
+    },
   });
+  if (args.version) {
+    command = 'version';
+  }
+  await commands[command]().then((module) => module.default(args));
 }
 
 start();
---
+++ src/version.ts
@@ -0,0 +1,3 @@
+export default function getVersion() {
+  return console.log('Changeloger version', process.env.npm_package_version);
+}
