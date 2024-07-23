6144d3be2ead55e7c5eeca8bc64cddbbfb981b2c
trandaison <sontd.it@gmail.com>
2024-07-23 13:25:36 +0700

    fix: version command returns undefined

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,15 @@
-export default function getVersion() {
-  return console.log('Changeloger version', process.env.npm_package_version);
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { PackageJson } from './lib/PackageJson';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default async function getVersion() {
+  const path = resolve(__dirname, '..');
+  const packageJson = new PackageJson(path);
+  await packageJson.load();
+  const version = packageJson.version.toString('', null);
+  console.log('Changeloger version', version);
+  return version;
 }
